a324a697083346d685663e7cd50887f9e01de4a7
huyencute1308-cmd <huyencute1308@gmail.com>
2025-12-01 19:09:24 +0700

    Add functionality to filter odd numbers from input

--- 4.4.25.py
+++ 4.4.25.py
@@ -1,2 +1,7 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+s = input("Nhập các số cách nhau bởi dấu phẩy: ")
+numbers = [int(x) for x in s.split(',')]
+odd_numbers = [x for x in numbers if x % 2 == 1]
+
+print("Các số lẻ:", ','.join(map(str, odd_numbers)))
+
+
