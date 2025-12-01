8ba9ccf37cb92ab67e070101f662a7e30bc4f05b
huyencute1308-cmd <huyencute1308@gmail.com>
2025-12-01 18:52:04 +0700

    Update 4.4.17.py

--- 4.4.17.py
+++ 4.4.17.py
@@ -1,2 +1,11 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+n = int(input("Nhập n: "))
+
+def sum_of_divisors(x):
+    return sum(i for i in range(1, x) if x % i == 0)
+
+for i in range(1, n):
+    if sum_of_divisors(i) > i:
+        print(i, end=' ')
+print()
+
+
