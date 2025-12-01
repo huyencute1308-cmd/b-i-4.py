083efbfa8f5dc29572bd157d0136ce92918aa506
huyencute1308-cmd <huyencute1308@gmail.com>
2025-12-01 19:05:39 +0700

    Add input processing for letters and digits count

--- 4.4.23.py
+++ 4.4.23.py
@@ -1,2 +1,7 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+s = input("Nhập câu: ")
+letters = sum(c.isalpha() for c in s)
+digits = sum(c.isdigit() for c in s)
+
+print("Số chữ cái:", letters)
+print("Số chữ số:", digits)
+
