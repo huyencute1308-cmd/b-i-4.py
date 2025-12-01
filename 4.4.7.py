dd5eb7a07c9dff99fcc2699ab444c213a138cd51
huyencute1308-cmd <huyencute1308@gmail.com>
2025-12-01 17:14:46 +0700

    Update 4.4.7.py

--- 4.4.7.py
+++ 4.4.7.py
@@ -1,2 +1,11 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+s = input("Nhập chuỗi: ")
+
+ket_qua = ""
+for ch in s:
+    if not ch.isdigit():     # nếu không phải chữ số
+        ket_qua += ch
+
+print("Chuỗi sau khi bỏ chữ số:", ket_qua)
+
+
+
