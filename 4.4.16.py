e3cd18378eb5452329814c2a2fe1ee0561e62500
huyencute1308-cmd <huyencute1308@gmail.com>
2025-12-01 18:47:20 +0700

    Update 4.4.16.py

--- 4.4.16.py
+++ 4.4.16.py
@@ -1,2 +1,5 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+s = input("Nhập các số nhị phân, cách nhau bởi dấu phẩy: ")
+binary_list = s.split(',')
+print("Các số vừa nhập:", binary_list)
+
+
