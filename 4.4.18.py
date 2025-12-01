30277106f6990576f6812bebdbb83626026e3dc1
huyencute1308-cmd <huyencute1308@gmail.com>
2025-12-01 18:54:06 +0700

    Update 4.4.18.py

--- 4.4.18.py
+++ 4.4.18.py
@@ -1,2 +1,8 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+n = int(input("Nhập n: "))
+
+fib = [0, 1]
+while fib[-1] + fib[-2] < n:
+    fib.append(fib[-1] + fib[-2])
+
+print("Các số Fibonacci nhỏ hơn n:", fib)
+
